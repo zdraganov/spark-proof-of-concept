6ddfb822b609a953b3ac8d4a1b9f408b6531ef19
kkalugerov <kkalugerov@gmail.com>
2017-05-31 16:53:36 +0300

    added spark stuffs

--- producer/producer.py
+++ producer/producer.py
@@ -2,12 +2,20 @@ import random
 import os
 import time
 import json
+import random
 from kafka import KafkaProducer
 
 kafka_host = os.environ['KAFKA_HOST']
 kafka_port = os.environ['KAFKA_PORT']
 
 raw_data_topic = os.environ['RAW_DATA_TOPIC']
+
+MESSAGE_TYPES = [ 'profile.picture.like' , 'profile.view' , 'message.private']
+START_INDEX = 1000
+END_INDEX = 101000
+
+def generate_random_message(start_index = START_INDEX , end_index = END_INDEX):
+	return (random.randint(start_index,end_index), random.choice(MESSAGE_TYPES))
 
 def main():
     time.sleep(20)
@@ -17,7 +25,7 @@ def main():
                              value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
     while True:
-        producer.send(raw_data_topic, {'bar': 'foo'})
+        producer.send(raw_data_topic, generate_random_message)
 
         print("Message generated")
         time.sleep(1)
---
+++ spark/spark-consumer.py
@@ -0,0 +1,34 @@
+#example build based on http://spark.apache.org/docs/2.1.0/api/python/pyspark.streaming.html and https://gist.github.com/rmoff/eadf82da8a0cd506c6c4a19ebd18037e
+
+from pyspark import SparkConf, SparkContext
+from operator import add
+import sys
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+from kafka import KafkaClient
+
+
+def main():
+
+	#create spark context
+	sc = SparkContext(appName="SparkStreamingWithKafka")
+
+	# create spark streaming context from spark context(sc) , second parameter is for batch duration
+	ssc = StreamingContext(sc,10)
+
+	# create kafka direct stream from spark streaming context and generated messeges from producer
+	kafka_stream = KafkaUtils.createStream(ssc, "localhost:2181", "metadata.broker.list": {raw_data_topic:1})
+
+	parsed = kafka_stream.map(lambda v: json.loads(v[1]))
+
+	#Count the number of instance of each profile view
+	profile_views = parsed.map(lambda views: (views['profile.view'],1)).\
+		reducedByKey(lambda x,y: x + y)
+
+	profile_views.pprint()
+
+	ssc.start()
+	ssc.awaitTermination()
+
+main()
